d7480ee4134e4f5a9cfb961ed15e70fb8b86aea5
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2022-10-07 11:32:12 +0300

    final version 1.03 add config.yml and method getToken

--- src/main/java/com/justeryt/discordbot/Main.java
+++ src/main/java/com/justeryt/discordbot/Main.java
@@ -3,6 +3,7 @@ package com.justeryt.discordbot;
 import com.justeryt.discordbot.commands.Listener.onJoin;
 import com.justeryt.discordbot.commands.commands.CommandManager;
 import com.justeryt.discordbot.commands.music.AudioManager;
+import com.justeryt.discordbot.resource.LoadToken;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
@@ -16,9 +17,7 @@ import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
-import javax.imageio.ImageIO;
 import javax.security.auth.login.LoginException;
-import java.io.File;
 import java.io.IOException;
 import java.util.Timer;
 
@@ -31,7 +30,7 @@ public class Main extends ListenerAdapter {
 
     public static void main(String[] args) throws IOException {
         //Вставляем ключ от бота (Нельзя проебать, а то заного его придется делать)
-        jdaBuilder = JDABuilder.createDefault("OTM2NTQxMTcxMjM5NTAxODM0.GbiLdr.kiOtev90S8lb6YdDtf0r5oZ-ys-ezxsq5hLuMw");
+        jdaBuilder = JDABuilder.createDefault(LoadToken.getToken());
         //Статус бота (Онлайн, спящий или оффлайн)
         jdaBuilder.setStatus(OnlineStatus.ONLINE);
         //Статус бота (Во что играет или просто цитатка)
---
+++ src/main/java/com/justeryt/discordbot/resource/LoadToken.java
@@ -0,0 +1,17 @@
+package com.justeryt.discordbot.resource;
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.*;
+
+public class LoadToken {
+
+    public static String getToken() throws FileNotFoundException {
+        String filename = "config.yml";
+        InputStream inputStream = new FileInputStream(new File(filename));
+
+        Yaml yaml = new Yaml();
+        String data = yaml.load(inputStream);
+        return data.split("token=")[1];
+    }
+}
