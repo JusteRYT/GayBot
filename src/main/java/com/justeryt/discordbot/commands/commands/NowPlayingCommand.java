3831579c2630ba078caad773bcdd45ed24f0a5d2
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2022-10-24 17:22:15 +0300

    v 1.04 Добавил команду !nowplay чтобы смотреть какой трек сейчас играет и получитьь ссылку на него

---
+++ src/main/java/com/justeryt/discordbot/commands/commands/NowPlayingCommand.java
@@ -0,0 +1,43 @@
+package com.justeryt.discordbot.commands.commands;
+
+import com.justeryt.discordbot.Main;
+import com.justeryt.discordbot.commands.Utils.EmbedCreate;
+import com.justeryt.discordbot.commands.music.MusicController;
+import com.justeryt.discordbot.commands.music.TrackScheduler;
+import com.justeryt.discordbot.commands.types.ServerCommand;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.api.entities.*;
+
+import java.util.ArrayList;
+
+public class NowPlayingCommand implements ServerCommand {
+    @Override
+    public void performCommand(String[] arguments, Guild guild, Member member, TextChannel textChannel, Message message, VoiceChannel voiceChannel) {
+        if (arguments.length == 1) {
+            if (voiceChannel != null) {
+                MusicController musicController = Main.getAudioManager().getMusicController(voiceChannel.getGuild().getIdLong());
+                AudioPlayer player = musicController.getAudioPlayer();
+                TrackScheduler scheduler = musicController.getScheduler();
+                player.addListener(scheduler);
+                if (player.getPlayingTrack() != null) {
+                    try {
+                        ArrayList<AudioTrack> list = scheduler.getList();
+                        EmbedCreate.createEmbedNowPlay("🔊Сейчас играет: " + player.getPlayingTrack().getInfo().title, Main.getIcon(),
+                                player.getPlayingTrack().getInfo().title, player.getPlayingTrack().getInfo().uri, list.size(), textChannel);
+                    } catch (IndexOutOfBoundsException e) {
+                        if (e.toString().startsWith("java.lang.IndexOutOfBoundsException: Index 0 out of bounds for length 0")) {
+                            EmbedCreate.createEmbed("📛В очереди нет треков, мудила, рукаблуд ссанина...", textChannel);
+                        }
+                    }
+                } else {
+                    EmbedCreate.createEmbed("📛В очереди нет треков, мудила, рукаблуд ссанина...", textChannel);
+                }
+            } else {
+                EmbedCreate.createEmbed("😡Я не в голосовом канале!!!", textChannel);
+            }
+        } else
+            EmbedCreate.createEmbed("📛Нужно писать вот так !nowplay", textChannel);
+    }
+}
+
