0488eeb0046fb2a3f3172502b9c2a19306d0b66f
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2022-10-24 17:19:35 +0300

    v 1.04 Передал комманду кика, чтобы не выдавал ошибки в консоль и команда кика работала

--- src/main/java/com/justeryt/discordbot/commands/commands/KickCommand.java
+++ src/main/java/com/justeryt/discordbot/commands/commands/KickCommand.java
@@ -1,5 +1,6 @@
 package com.justeryt.discordbot.commands.commands;
 
+import com.justeryt.discordbot.Main;
 import com.justeryt.discordbot.commands.Utils.EmbedCreate;
 import com.justeryt.discordbot.commands.types.ServerCommand;
 import net.dv8tion.jda.api.Permission;
@@ -8,20 +9,32 @@ import net.dv8tion.jda.api.entities.*;
 public class KickCommand implements ServerCommand {
 
     @Override
-    public void performCommand(String[] arguments, Guild guild, Member member, TextChannel textChannel, Message message, VoiceChannel voiceChannel) {
+    public void performCommand(String[] arguments, Guild guild, Member member, TextChannel textChannel, Message message, VoiceChannel voiceChannel)  {
         if (arguments.length == 3) {
-            Member target = message.getMentionedMembers().get(0);
-            if (target != null){
-                String reason = arguments[2];
-                if (reason != null ){
-                    if (member.hasPermission(Permission.KICK_MEMBERS)){
-                        EmbedCreate.createEmbed("✅Ну пока клоун по имени: " + target.getUser().getName() + ". Причина кика: " + reason, textChannel);
-                        target.kick(reason).queue();
+            try {
+                Member target = message.getMentionedMembers().get(0);
+                if (target != null) {
+                    String reason = arguments[2];
+                    if (reason != null) {
+                        if (member.hasPermission(Permission.KICK_MEMBERS)) {
+                            EmbedCreate.createEmbed("✅Ну пока клоун по имени: " + target.getUser().getName() + ". Причина кика: " + reason, textChannel);
+                            guild.kick(target).queue();
+                        } else {
+                            EmbedCreate.createEmbed("⛔У меня нет прав на данное действие!", textChannel);
+                        }
                     }
                 }
+            } catch (IndexOutOfBoundsException e) {
+                if (e.toString().startsWith("java.lang.IndexOutOfBoundsException: Index 0 out of bounds for length 0")) {
+                    EmbedCreate.createEmbed("📛Неверный пользователь!", textChannel);
+                }
             }
+        } else if (arguments.length == 2) {
+            EmbedCreate.createEmbed("⛔Введите причину кика!", textChannel);
+        } else if (arguments.length == 1) {
+            EmbedCreate.createEmbed("⛔Введите кого кикать!", textChannel);
         } else {
-            EmbedCreate.createEmbed("⛔Введите кого кикать и причину",textChannel);
+            EmbedCreate.createEmbed("⛔Введите кого кикать и причину!", textChannel);
         }
     }
 }
