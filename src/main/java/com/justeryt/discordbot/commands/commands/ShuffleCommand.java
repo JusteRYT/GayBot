fdc23c90774eff8f43d2534bbf23ef59fb54bfc3
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2022-10-24 17:26:59 +0300

    v 1.04 Перемешивание треков

--- src/main/java/com/justeryt/discordbot/commands/commands/ShuffleCommand.java
+++ src/main/java/com/justeryt/discordbot/commands/commands/ShuffleCommand.java
@@ -6,22 +6,38 @@ import com.justeryt.discordbot.commands.types.ServerCommand;
 import com.justeryt.discordbot.commands.music.MusicController;
 import com.justeryt.discordbot.commands.music.TrackScheduler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.api.entities.*;
+
+import java.util.ArrayList;
 
 public class ShuffleCommand implements ServerCommand {
 
 
     @Override
     public void performCommand(String[] arguments, Guild guild, Member member, TextChannel textChannel, Message message, VoiceChannel voiceChannel) {
-        if (voiceChannel != null) {
-            MusicController musicController = Main.getAudioManager().getMusicController(voiceChannel.getGuild().getIdLong());
-            AudioPlayer player = musicController.getAudioPlayer();
-            TrackScheduler scheduler = musicController.getScheduler();
-            player.addListener(scheduler);
-            scheduler.shuffle();
-            EmbedCreate.createEmbed("✅Плейлист перемешен", textChannel);
+        if (arguments.length == 1) {
+            if (voiceChannel != null) {
+                MusicController musicController = Main.getAudioManager().getMusicController(voiceChannel.getGuild().getIdLong());
+                AudioPlayer player = musicController.getAudioPlayer();
+                TrackScheduler scheduler = musicController.getScheduler();
+                player.addListener(scheduler);
+                ArrayList<AudioTrack> list = scheduler.getList();
+                try {
+                    if (list.get(0) != null) {
+                        scheduler.shuffle();
+                        EmbedCreate.createEmbed("✅Плейлист перемешен", textChannel);
+                    } else {
+                        EmbedCreate.createEmbed("📛В очереди нет треков, так что я не буду нечего мешать!!!", textChannel);
+                    }
+                } catch (IndexOutOfBoundsException e) {
+                    EmbedCreate.createEmbed("📛В очереди нет треков, так что я не буду нечего мешать!!!", textChannel);
+                }
+            } else {
+                EmbedCreate.createEmbed("📛А ну да, мне же надо сначало воспроизвести музыку, а потом подключаться, это ты здорово придумал", textChannel);
+            }
         } else {
-            EmbedCreate.createEmbed("📛А ну да, мне же надо сначало воспроизвести музыку, а потом подключаться, это ты здорово придумал", textChannel);
+            EmbedCreate.createEmbed("🤦‍♂️Напиши вот так и не беси меня !shuffle", textChannel);
         }
     }
 }
