18ed85ad5012ddbcecda659b5b96243c360eb498
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2022-10-24 17:27:21 +0300

    v 1.04 Настроил обработчик ошибок

--- src/main/java/com/justeryt/discordbot/commands/commands/SkipCommand.java
+++ src/main/java/com/justeryt/discordbot/commands/commands/SkipCommand.java
@@ -6,21 +6,30 @@ import com.justeryt.discordbot.commands.types.ServerCommand;
 import com.justeryt.discordbot.commands.music.MusicController;
 import com.justeryt.discordbot.commands.music.TrackScheduler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.api.entities.*;
+
+import java.util.ArrayList;
 
 public class SkipCommand implements ServerCommand {
     @Override
     public void performCommand(String[] arguments, Guild guild, Member member, TextChannel textChannel, Message message, VoiceChannel voiceChannel) {
-        if (voiceChannel != null) {
-            assert Main.getAudioManager() != null;
-            MusicController musicController = Main.getAudioManager().getMusicController(voiceChannel.getGuild().getIdLong());
-            AudioPlayer player = musicController.getAudioPlayer();
-            TrackScheduler scheduler = musicController.getScheduler();
-            player.addListener(scheduler);
-            scheduler.skip();
-            EmbedCreate.createEmbed("✅Трек пропущен", textChannel);
-        } else {
-            EmbedCreate.createEmbed("📛Я не в голосовом канале мудак!", textChannel);
+        if (arguments.length == 1) {
+            if (voiceChannel != null) {
+                MusicController musicController = Main.getAudioManager().getMusicController(voiceChannel.getGuild().getIdLong());
+                AudioPlayer player = musicController.getAudioPlayer();
+                TrackScheduler scheduler = musicController.getScheduler();
+                player.addListener(scheduler);
+                ArrayList<AudioTrack> list = scheduler.getList();
+                if (!list.isEmpty() || player.getPlayingTrack() != null) {
+                    scheduler.skip();
+                    EmbedCreate.createEmbed("✅Трек пропущен", textChannel);
+                } else {
+                    EmbedCreate.createEmbed("💢Нечего скипать еблан!", textChannel);
+                }
+            } else {
+                EmbedCreate.createEmbed("📛Я не в голосовом канале мудак!", textChannel);
+            }
         }
     }
 }
