18ad722d5dedaf90b83aed06b3dcc62fd0e03a3e
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2022-10-24 17:30:36 +0300

    v 1.04 Переработал комманду чтобы при !volume он выводил текущую громкость, !volume 0-100 показывал смайлик по громкости

--- src/main/java/com/justeryt/discordbot/commands/commands/VolumeCommand.java
+++ src/main/java/com/justeryt/discordbot/commands/commands/VolumeCommand.java
@@ -11,26 +11,37 @@ import net.dv8tion.jda.api.entities.*;
 public class VolumeCommand implements ServerCommand {
     @Override
     public void performCommand(String[] arguments, Guild guild, Member member, TextChannel textChannel, Message message, VoiceChannel voiceChannel) {
-        int volume = Integer.parseInt(arguments[1]);
-        if (arguments.length == 2) {
-            if (voiceChannel != null) {
+        try {
+            if (arguments.length == 2) {
+                if (voiceChannel != null) {
+                    MusicController musicController = Main.getAudioManager().getMusicController(voiceChannel.getGuild().getIdLong());
+                    AudioPlayer player = musicController.getAudioPlayer();
+                    TrackScheduler scheduler = musicController.getScheduler();
+                    player.addListener(scheduler);
+                    int volume = Integer.parseInt(arguments[1]);
+                    scheduler.setVolume(volume);
+                    if (volume >= 50 & volume <= 100) {
+                        EmbedCreate.createEmbed("🔊Громкость вот такая: " + volume, textChannel);
+                    }
+                    if (volume <= 49 & volume >= 20) {
+                        EmbedCreate.createEmbed("🔉Громкость вот такая: " + volume, textChannel);
+                    }
+                    if (volume <= 19 & volume >= 0) {
+                        EmbedCreate.createEmbed("🔈Громкость вот такая: " + volume, textChannel);
+                    }
+                } else {
+                    EmbedCreate.createEmbed("📛Я не в голосовом канале, ебень", textChannel);
+                }
+            } else if (arguments.length == 1) {
                 MusicController musicController = Main.getAudioManager().getMusicController(voiceChannel.getGuild().getIdLong());
                 AudioPlayer player = musicController.getAudioPlayer();
-                TrackScheduler scheduler = musicController.getScheduler();
-                player.addListener(scheduler);
-                scheduler.setVolume(volume);
-                if (volume >= 50 & volume <= 100) {
-                    EmbedCreate.createEmbed("🔊Громкость вот такая: " + volume, textChannel);
-                }
-                if (volume <= 49 & volume >= 20) {
-                    EmbedCreate.createEmbed("🔉Громкость вот такая: " + volume, textChannel);
-                }
-                if (volume <= 19 & volume >= 0) {
-                    EmbedCreate.createEmbed("🔈Громкость вот такая: " + volume, textChannel);
-                }
+                int volume = player.getVolume();
+                EmbedCreate.createEmbed("🔊Громкость вот такая: " + volume, textChannel);
             } else {
-                EmbedCreate.createEmbed("📛Я не в голосовом канале, ебень", textChannel);
+                EmbedCreate.createEmbed("😡Пиши вот так чмо !volume 0-100", textChannel);
             }
+        } catch (NumberFormatException e) {
+            EmbedCreate.createEmbed("Ну ты еблан, цыфры от букв не отличаешь 🤦‍♂️", textChannel);
         }
     }
 }
