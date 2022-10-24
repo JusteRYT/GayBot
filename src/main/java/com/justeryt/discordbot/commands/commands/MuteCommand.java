642cbcbd9385b48c392c386a6c7c3df23b2bd928
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2022-10-24 17:21:44 +0300

    v 1.04 Добавил команду мута в голосовом канале

---
+++ src/main/java/com/justeryt/discordbot/commands/commands/MuteCommand.java
@@ -0,0 +1,28 @@
+package com.justeryt.discordbot.commands.commands;
+
+import com.justeryt.discordbot.commands.Utils.EmbedCreate;
+import com.justeryt.discordbot.commands.types.ServerCommand;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.exceptions.ContextException;
+
+public class MuteCommand implements ServerCommand {
+    @Override
+    public void performCommand(String[] arguments, Guild guild, Member member, TextChannel textChannel, Message message, VoiceChannel voiceChannel)  {
+        if (arguments.length == 2) {
+            try {
+                Member target = message.getMentionedMembers().get(0);
+                if (target != null) {
+                    if(member.hasPermission(Permission.VOICE_MUTE_OTHERS)){
+                        EmbedCreate.createEmbed("☺Я замутил этого еблана: " + target.getUser().getName(), textChannel);
+                        target.mute(true).queue();
+                    }
+                }
+            } catch (IndexOutOfBoundsException e) {
+                if (e.toString().startsWith("java.lang.IndexOutOfBoundsException: Index 0 out of bounds for length 0")) {
+                    EmbedCreate.createEmbed("📛Неверный пользователь!", textChannel);
+                }
+            }
+        }
+    }
+}
