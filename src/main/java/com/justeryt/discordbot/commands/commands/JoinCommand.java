7b17144ed0d30e166dbc7328673412797dfcc139
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2022-10-24 17:18:46 +0300

    v 1.04 add new command !join

---
+++ src/main/java/com/justeryt/discordbot/commands/commands/JoinCommand.java
@@ -0,0 +1,27 @@
+package com.justeryt.discordbot.commands.commands;
+
+import com.justeryt.discordbot.commands.Utils.EmbedCreate;
+import com.justeryt.discordbot.commands.types.ServerCommand;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.managers.AudioManager;
+
+public class JoinCommand implements ServerCommand {
+    @Override
+    public void performCommand(String[] arguments, Guild guild, Member member, TextChannel textChannel, Message message, VoiceChannel voiceChannel) {
+        GuildVoiceState guildVoiceState;
+        if (arguments.length == 1) {
+            if ((guildVoiceState = member.getVoiceState()) != null) {
+                if ((voiceChannel = guildVoiceState.getChannel()) != null) {
+                    AudioManager audioManager = guildVoiceState.getGuild().getAudioManager();
+                    audioManager.openAudioConnection(voiceChannel);
+                    EmbedCreate.createEmbed("😅Я подключился к каналу: " + voiceChannel.getName(), textChannel);
+                } else {
+                    EmbedCreate.createEmbed("😡Ты не в голосовом канале сучка!", textChannel);
+                }
+            }
+        } else {
+            EmbedCreate.createEmbed("📛Ну напиши ты корявыми пальцами вот так !join, не !join хуебес быстро блять," +
+                    "а просто !join", textChannel);
+        }
+    }
+}
