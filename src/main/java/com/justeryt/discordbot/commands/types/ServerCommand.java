a3bc9e3165fb847bc961582526d8ebe62184849c
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2022-10-24 17:26:35 +0300

    v 1.04

--- src/main/java/com/justeryt/discordbot/commands/types/ServerCommand.java
+++ src/main/java/com/justeryt/discordbot/commands/types/ServerCommand.java
@@ -1,7 +1,6 @@
 package com.justeryt.discordbot.commands.types;
 
 import net.dv8tion.jda.api.entities.*;
-
 public interface ServerCommand {
     public void performCommand(String[] arguments, Guild guild, Member member, TextChannel textChannel, Message message, VoiceChannel voiceChannel);
 }
