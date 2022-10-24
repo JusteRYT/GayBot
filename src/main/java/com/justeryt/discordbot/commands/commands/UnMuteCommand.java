aa1cd81b00cff59de4982da6f9cec87c4e67dc87
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2022-10-24 17:28:56 +0300

    v 1.04 Команда размучивать пользователей, дополнение к !mute

---
+++ src/main/java/com/justeryt/discordbot/commands/commands/UnMuteCommand.java
@@ -0,0 +1,27 @@
+package com.justeryt.discordbot.commands.commands;
+
+import com.justeryt.discordbot.commands.Utils.EmbedCreate;
+import com.justeryt.discordbot.commands.types.ServerCommand;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.*;
+
+public class UnMuteCommand implements ServerCommand {
+    @Override
+    public void performCommand(String[] arguments, Guild guild, Member member, TextChannel textChannel, Message message, VoiceChannel voiceChannel)  {
+        if (arguments.length == 2) {
+            try {
+                Member target = message.getMentionedMembers().get(0);
+                if (target != null) {
+                    if(member.hasPermission(Permission.VOICE_MUTE_OTHERS)){
+                        EmbedCreate.createEmbed("👍Я размутил его: " + target.getUser().getName() +", но это в последний раз", textChannel);
+                        target.mute(false).queue();
+                    }
+                }
+            } catch (IndexOutOfBoundsException e) {
+                if (e.toString().startsWith("java.lang.IndexOutOfBoundsException: Index 0 out of bounds for length 0")) {
+                    EmbedCreate.createEmbed("📛Неверный пользователь!", textChannel);
+                }
+            }
+        }
+    }
+}
