3cb94066f254fd35ad674c3f5f10dcd226510fe2
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2022-10-24 17:29:25 +0300

    v 1.04 Добавление пару инфы о пользователей

--- src/main/java/com/justeryt/discordbot/commands/commands/UserInfoCommand.java
+++ src/main/java/com/justeryt/discordbot/commands/commands/UserInfoCommand.java
@@ -2,42 +2,41 @@ package com.justeryt.discordbot.commands.commands;
 
 import com.justeryt.discordbot.commands.Utils.EmbedCreate;
 import com.justeryt.discordbot.commands.types.ServerCommand;
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.*;
 
-import java.awt.*;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class UserInfoCommand implements ServerCommand {
     @Override
     public void performCommand(String[] arguments, Guild guild, Member member, TextChannel textChannel, Message message, VoiceChannel voiceChannel) {
-        if(arguments.length == 2) {
-            Member target = message.getMentionedMembers().get(0);
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-            if(target != null) {
-                EmbedCreate.createEmbedUserInfo("Чел - " + target.getUser().getName(),target.getUser().getAvatarUrl(),
-                        (target.getTimeJoined().format(formatter)),(target.getTimeCreated().format(formatter)),
-                        String.valueOf(checkForBot(target)),getRoles(target.getRoles()), textChannel );
-            } else {
-                EmbedCreate.createEmbed("🤡!info <Чел>",textChannel);
-            }
+        if (arguments.length == 2) {
+                Member target = message.getMentionedMembers().get(0);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+                if (target != null) {
+                    EmbedCreate.createEmbedUserInfo("Чел - " + target.getUser().getName(), target.getUser().getAvatarUrl(),
+                            (target.getTimeJoined().format(formatter)), (target.getTimeCreated().format(formatter)),
+                            checkForBot(target), getRoles(target.getRoles()), target.getOnlineStatus(),
+                            textChannel);
+                }
+        } else {
+            EmbedCreate.createEmbed("🤡!info <Чел>", textChannel);
         }
     }
 
     private String checkForBot(Member member) {
-        if(member.getUser().isBot()) {
+        if (member.getUser().isBot()) {
             return "💥Ботяра ебаная";
-        }else {
+        } else {
             return "☺Пчел";
         }
     }
 
-    private String getRoles(List rolesList){
+    private String getRoles(List rolesList) {
         String roles = "";
         if (!rolesList.isEmpty()) {
             Role tempRole = (Role) rolesList.get(0);
-            for (int i = 0; i < rolesList.size(); i ++){
+            for (int i = 0; i < rolesList.size(); i++) {
                 tempRole = (Role) rolesList.get(i);
                 roles = tempRole.getName();
             }
