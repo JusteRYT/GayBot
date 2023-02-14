b0d4711fbce91e5249e760777588d5c2354b43d2
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2023-02-14 11:36:53 +0300

    v 1.13 Added a new command to view the number of servers that the bot is using

---
+++ src/main/java/com/justeryt/discordbot/commands/commands/HowManyGuildCommand.java
@@ -0,0 +1,20 @@
+package com.justeryt.discordbot.commands.commands;
+
+import com.justeryt.discordbot.Main;
+import com.justeryt.discordbot.commands.Utils.EmbedCreate;
+import com.justeryt.discordbot.commands.types.ServerCommand;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+
+public class HowManyGuildCommand implements ServerCommand {
+    @Override
+    public void performCommand(String[] arguments, Guild guild, Member member, MessageChannel textChannel, Message message, AudioChannel voiceChannel) {
+        if (arguments.length == 1) {
+            int size = Main.getJda().getGuilds().size();
+            EmbedCreate.createEmbed("Вот количество серверов, на которых находится данный бот: " + size, textChannel);
+        }
+    }
+}
