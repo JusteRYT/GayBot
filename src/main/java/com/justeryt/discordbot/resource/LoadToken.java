5655ff07b3d305a67ed22caf284e31309196c9d6
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2023-02-14 11:37:27 +0300

    v 1.13 Changed yaml reading methods, added method for YouTube key

--- src/main/java/com/justeryt/discordbot/resource/LoadToken.java
+++ src/main/java/com/justeryt/discordbot/resource/LoadToken.java
@@ -3,15 +3,20 @@ package com.justeryt.discordbot.resource;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.*;
+import java.util.Map;
 
 public class LoadToken {
+    public static String filename = "testconfig.yml";
+    public static Yaml yaml = new Yaml();
 
     public static String getToken() throws FileNotFoundException {
-        String filename = "config.yml";
-        InputStream inputStream = new FileInputStream(new File(filename));
-
-        Yaml yaml = new Yaml();
-        String data = yaml.load(inputStream);
-        return data.split("token=")[1];
+        InputStream inputStream = new FileInputStream(filename);
+        Map data = yaml.load(inputStream);
+        return data.get("token").toString();
+    }
+    public static String getApiKey() throws FileNotFoundException {
+        InputStream inputStream = new FileInputStream(filename);
+        Map data = yaml.load(inputStream);
+        return data.get("ApiToken").toString();
     }
 }
