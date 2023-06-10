2a5a6e5c59c1ae26f323f0183284063bf937257c
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2023-06-10 22:44:52 +0300

    v 1.14 Slightly changed, but in the future I will fix the appearance and algorithm

--- src/main/java/com/justeryt/discordbot/commands/commands/HistoryCommand.java
+++ src/main/java/com/justeryt/discordbot/commands/commands/HistoryCommand.java
@@ -1,7 +1,5 @@
 package com.justeryt.discordbot.commands.commands;
 
-import com.justeryt.discordbot.Main;
-import com.justeryt.discordbot.commands.Utils.EmbedCreate;
 import com.justeryt.discordbot.commands.music.TrackScheduler;
 import com.justeryt.discordbot.commands.types.ServerCommand;
 import net.dv8tion.jda.api.entities.Guild;
@@ -10,23 +8,11 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 
-import java.util.ArrayList;
-
 public class HistoryCommand implements ServerCommand {
     @Override
     public void performCommand(String[] arguments, Guild guild, Member member, MessageChannel textChannel, Message message, AudioChannel voiceChannel) {
         if (arguments.length == 1) {
-            ArrayList<String> name = TrackScheduler.getTitle();
-            StringBuilder stringBuilder = Main.getStringBuilder();
-            if (name != null) {
-                int k = 1;
-                for (int i = name.size() - 1; i > -1; i--, k++) {
-                    stringBuilder.append(String.format("Трек %d: %s\n", k, name.get(i)));
-                }
-                EmbedCreate.createHistoryEmbed(stringBuilder.toString(), textChannel);
-            } else {
-                EmbedCreate.createEmbed("Лист пустой!", textChannel);
-            }
+            TrackScheduler.history(textChannel);
         }
     }
 }
