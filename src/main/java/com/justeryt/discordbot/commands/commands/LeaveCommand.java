b45d5625d867ba1d9754d036ef17f4fd5eef1bd4
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2023-06-10 22:46:16 +0300

    v 1.14 Now when the bot leaves the channel it deletes the queue

--- src/main/java/com/justeryt/discordbot/commands/commands/LeaveCommand.java
+++ src/main/java/com/justeryt/discordbot/commands/commands/LeaveCommand.java
@@ -1,6 +1,8 @@
 package com.justeryt.discordbot.commands.commands;
 
+import com.justeryt.discordbot.Main;
 import com.justeryt.discordbot.commands.Utils.EmbedCreate;
+import com.justeryt.discordbot.commands.music.TrackScheduler;
 import com.justeryt.discordbot.commands.types.ServerCommand;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
@@ -14,6 +16,7 @@ public class LeaveCommand implements ServerCommand {
         }else {
             guild.getAudioManager().closeAudioConnection();
             EmbedCreate.createEmbed("🙋Я ливнул пидорасы!!!", textChannel);
+            Main.getAudioManager().getMusicController(voiceChannel.getGuild().getIdLong()).getScheduler().drainQueue();
         }
 
     }
