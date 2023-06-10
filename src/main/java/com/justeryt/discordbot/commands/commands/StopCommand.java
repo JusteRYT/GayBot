b26662333aded55c4340418183af7fa9ce5cf7cd
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2023-06-10 22:49:18 +0300

    v 1.14 Fixed the algorithm

--- src/main/java/com/justeryt/discordbot/commands/commands/StopCommand.java
+++ src/main/java/com/justeryt/discordbot/commands/commands/StopCommand.java
@@ -6,34 +6,21 @@ import com.justeryt.discordbot.commands.music.MusicController;
 import com.justeryt.discordbot.commands.music.TrackScheduler;
 import com.justeryt.discordbot.commands.types.ServerCommand;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
-import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 
 public class StopCommand implements ServerCommand {
     @Override
     public void performCommand(String[] arguments, Guild guild, Member member, MessageChannel textChannel, Message message, AudioChannel voiceChannel) {
-        if (arguments.length == 1) {
-            if (voiceChannel != null) {
-                MusicController musicController = Main.getAudioManager().getMusicController(voiceChannel.getGuild().getIdLong());
-                AudioPlayer player = musicController.getAudioPlayer();
-                TrackScheduler scheduler = musicController.getScheduler();
-                player.addListener(scheduler);
-                if (player.getPlayingTrack() != null) {
-                    if (!player.isPaused()) {
-                        scheduler.stop();
-                        EmbedCreate.createEmbed("💢Мы его задержали", textChannel);
-                    } else {
-                        EmbedCreate.createEmbed("📛Он и так не играет дэбил!!!", textChannel);
-                    }
-                } else {
-                    EmbedCreate.createEmbed("😡В очереди нет треков", textChannel);
-                }
-            } else {
-                EmbedCreate.createEmbed("📛Я не в голосовом канале мудак!", textChannel);
-            }
-        } else {
-            EmbedCreate.createEmbed("🤦‍♂️Ни как вы блять не научитесь !stop и всё!!!", textChannel);
-        }
+        MusicController musicController = Main.getAudioManager().getMusicController(voiceChannel.getGuild().getIdLong());
+        AudioPlayer player = musicController.getAudioPlayer();
+        TrackScheduler scheduler = musicController.getScheduler();
+        player.addListener(scheduler);
+        player.stopTrack();
+        scheduler.drainQueue();
+        EmbedCreate.createEmbed("Стоп!", textChannel);
     }
 }
