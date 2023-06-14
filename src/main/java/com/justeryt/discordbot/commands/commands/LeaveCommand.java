3017b761faed9f97d064378efba77fc816604a9e
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2023-06-14 20:45:09 +0300

    v 1.14.1 added a check that when the bot leaves the server, the queue of songs is deleted

--- src/main/java/com/justeryt/discordbot/commands/commands/LeaveCommand.java
+++ src/main/java/com/justeryt/discordbot/commands/commands/LeaveCommand.java
@@ -2,8 +2,10 @@ package com.justeryt.discordbot.commands.commands;
 
 import com.justeryt.discordbot.Main;
 import com.justeryt.discordbot.commands.Utils.EmbedCreate;
+import com.justeryt.discordbot.commands.music.MusicController;
 import com.justeryt.discordbot.commands.music.TrackScheduler;
 import com.justeryt.discordbot.commands.types.ServerCommand;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
@@ -14,9 +16,15 @@ public class LeaveCommand implements ServerCommand {
         if (voiceChannel == null){
             EmbedCreate.createEmbed("📛Ебанат я не в голосовом чате", textChannel);
         }else {
+            MusicController musicController = Main.getAudioManager().getMusicController(voiceChannel.getGuild().getIdLong());
+            AudioPlayer player = musicController.getAudioPlayer();
+            TrackScheduler scheduler = musicController.getScheduler();
+            player.addListener(scheduler);
+            player.stopTrack();
+            scheduler.drainQueue();
             guild.getAudioManager().closeAudioConnection();
             EmbedCreate.createEmbed("🙋Я ливнул пидорасы!!!", textChannel);
-            Main.getAudioManager().getMusicController(voiceChannel.getGuild().getIdLong()).getScheduler().drainQueue();
+
         }
 
     }
