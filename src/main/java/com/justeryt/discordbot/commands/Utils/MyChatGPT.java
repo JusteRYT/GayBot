85ac0abddb7ee22d98c4f727909222c99f6441c1
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2023-06-19 12:44:41 +0300

    v 1.15.1 add animated write

--- src/main/java/com/justeryt/discordbot/commands/Utils/MyChatGPT.java
+++ src/main/java/com/justeryt/discordbot/commands/Utils/MyChatGPT.java
@@ -1,14 +1,22 @@
 package com.justeryt.discordbot.commands.Utils;
 
+import com.justeryt.discordbot.resource.LoadToken;
 import com.plexpt.chatgpt.ChatGPT;
+
+import java.io.FileNotFoundException;
 
 public class MyChatGPT {
 
     public static String generateChatGptResponse(String message) {
-        ChatGPT chatGPT = ChatGPT.builder()
-                .apiKey("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")
-                .build()
-                .init();
+        ChatGPT chatGPT = null;
+        try {
+            chatGPT = ChatGPT.builder()
+                    .apiKey(LoadToken.getApiChatGPT())
+                    .build()
+                    .init();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         return chatGPT.chat(message);
     }
 }
