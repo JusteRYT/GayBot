c2a759d0e8eb4d4bc98db0364152cd275b930609
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2023-06-19 12:44:59 +0300

    v 1.15.1 add change button

--- src/main/java/com/justeryt/discordbot/commands/commands/PlayOrNot.java
+++ src/main/java/com/justeryt/discordbot/commands/commands/PlayOrNot.java
@@ -11,12 +11,19 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+
+import java.util.concurrent.TimeUnit;
 
 public class PlayOrNot implements ServerCommand {
+    static ButtonInteractionEvent event;
     @Override
     public void performCommand(String[] arguments, Guild guild, Member member, MessageChannel textChannel, Message message, AudioChannel voiceChannel) {
         if (voiceChannel != null) {
-
+            Guild emojiGuild = Main.getJda().getGuildById(403639391265882115L);
+            Emoji play = emojiGuild.getEmojisByName("play", true).get(0);
+            Emoji pause = emojiGuild.getEmojisByName("pause", true).get(0);
             MusicController musicController = Main.getAudioManager().getMusicController(voiceChannel.getGuild().getIdLong());
             AudioPlayer player = musicController.getAudioPlayer();
             TrackScheduler scheduler = musicController.getScheduler();
@@ -24,12 +31,23 @@ public class PlayOrNot implements ServerCommand {
             if (player.getPlayingTrack() != null) {
                 if (player.isPaused()) {
                     scheduler.resume();
+                    event.editButton(event.getButton().withEmoji(pause)).queue();
+
                 } else if (!player.isPaused()){
                     scheduler.pause();
+                    event.editButton(event.getButton().withEmoji(play)).queue();
+                    
                 }
             } else {
                 EmbedCreate.createEmbed("В очереди нет треков📛",textChannel);
             }
         }
+    }
+    public static void setEvent(ButtonInteractionEvent event){
+        PlayOrNot.event = event;
+    }
+
+    public ButtonInteractionEvent getEvent() {
+        return event;
     }
 }
