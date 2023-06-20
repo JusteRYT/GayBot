340f184e6e2f4301527734d31ac7c42b7af9294a
JusteRYT <93572822+JusteRYT@users.noreply.github.com>
2023-06-20 09:00:30 +0300

    v 1.15.1 Added check

--- src/main/java/com/justeryt/discordbot/commands/commands/GPT.java
+++ src/main/java/com/justeryt/discordbot/commands/commands/GPT.java
@@ -11,26 +11,41 @@ import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 
 
 public class GPT implements ServerCommand {
+    private static final int MAX_CONTENT_LENGTH = 2000;
     @Override
     public void performCommand(String[] arguments, Guild guild, Member member, MessageChannel textChannel, Message message, AudioChannel voiceChannel) {
         StringBuilder builder = Main.getStringBuilder();
-        for (int i = 1; i < arguments.length; i++) builder.append(arguments[i]).append(" ");
+        for (int i = 1; i < arguments.length; i++) {
+            builder.append(arguments[i]).append(" ");
+        }
         String response = builder.toString().trim().replace("]", "").replace("[", "");
-       String generatedResponse = MyChatGPT.generateChatGptResponse(response);
-        String[] responseParts = generatedResponse.split(" ");
-        Message sentMessage = textChannel.sendMessage("...").complete();
-        for (String word : responseParts){
-            builder.append(word).append(" ");
-            sentMessage.editMessage(builder.toString()).submit();
-            try{
-                int delay = word.length() * 1000;
-                Thread.sleep(delay);
-            }catch (InterruptedException e){
-                textChannel.sendMessage(e.getMessage()).queue();
+        String generatedResponse = MyChatGPT.generateChatGptResponse(response);
+        sendGeneratedResponse(generatedResponse, textChannel);
+    }
+
+    //textChannel.sendMessage(MyChatGPT.generateChatGptResponse(response)).queue(message1 -> message1.delete().submitAfter(60, TimeUnit.SECONDS));
+    private void sendGeneratedResponse(String generatedResponse, MessageChannel channel) {
+        if (generatedResponse.length() <= MAX_CONTENT_LENGTH) {
+            channel.sendMessage(generatedResponse).queue();
+        } else {
+            int startIndex = 0;
+            int endIndex = MAX_CONTENT_LENGTH;
+            while (startIndex < generatedResponse.length()) {
+                if (endIndex >= generatedResponse.length()) {
+                    endIndex = generatedResponse.length();
+                } else {
+                    while (endIndex > startIndex && !Character.isWhitespace(generatedResponse.charAt(endIndex))) {
+                        endIndex--;
+                    }
+                }
+
+                String messagePart = generatedResponse.substring(startIndex, endIndex);
+                channel.sendMessage(messagePart).queue();
+
+                startIndex = endIndex;
+                endIndex += MAX_CONTENT_LENGTH;
             }
         }
-        sentMessage.editMessage(builder.toString().replace("...","")).queue();
-        //textChannel.sendMessage(MyChatGPT.generateChatGptResponse(response)).queue(message1 -> message1.delete().submitAfter(60, TimeUnit.SECONDS));
     }
 }
 
